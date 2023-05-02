8a377356b4a1937fb015b93d54e4b44177e255de
Affan <affan.habib44@gmail.com>
2023-05-02 21:58:42 +0600

    updated

---
+++ swagger-jsdoc.js
@@ -0,0 +1,28 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *         name:
+ *           type: string
+ *       required:
+ *         - name
+ * 
+ * /users:
+ *   get:
+ *     summary: Returns a list of users
+ *     description: Optional extended description in Markdown.
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
